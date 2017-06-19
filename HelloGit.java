8f3a763b50237dd0462a76391da4d15ec3c36c1b
PeterMartinez <peter.m.martinez92@gmail.com>
2017-06-19 17:48:35 -0400

    Back at 3 lines

--- HelloGit.java
+++ HelloGit.java
@@ -2,14 +2,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Git (and Java), nice to meet you!");
-<<<<<<< HEAD
 		System.out.println("Hello again, am I doing this right?");
 		System.out.println("This is my third commit");
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of 2f5d187... Added a second greeting
->>>>>>> parent of 28970a9... Back to three lines
+
 
 	}
 }
