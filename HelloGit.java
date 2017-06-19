2f5d187160887957d280f3e3e8d3ecdf91b02b59
PeterMartinez <peter.m.martinez92@gmail.com>
2017-06-19 17:26:39 -0400

    Added a second greeting

--- HelloGit.java
+++ HelloGit.java
@@ -2,6 +2,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Git (and Java), nice to meet you!");
+		System.out.println("Hello again, am I doing this right?");
 
 	}
 }
